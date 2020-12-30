27ae84bf23567741e962d3dfeddd3ba4a9861152
Michelle <michellexia@utexas.edu>
2020-12-29 21:13:43 -0600

    #46 Permutations

---
+++ Permutations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums):
+        out = []
+        
+        if len(nums) == 1:
+            return [nums[:]]
+        
+        for i in range(len(nums)):
+            val = nums.pop(0)
+            permutations = self.permute(nums)
+
+            for permutation in permutations:
+                permutation.append(val)
+
+            out.extend(permutations)
+            nums.append(val)
+        
+        return out 
