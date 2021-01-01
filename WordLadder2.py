d73e72a4e0f7e07aafc9d7674cb31ff9820d9051
Michelle <michellexia@utexas.edu>
2021-01-01 14:03:40 -0600

    #126 Word Ladder 2

--- WordLadder2.py
+++ WordLadder2.py
@@ -1,59 +1,54 @@
+from collections import defaultdict
+
 class Solution:
     def findLadders(self, beginWord, endWord, wordList):
         # faster lookup time
         wordList = set(wordList)
         
-        # initialize adjacency list with parent:child
-        adjacency_map = {} 
+        # initialize adjacency list with parent:child layers
+        adjacency_map = defaultdict(set)
         
-        # create list to track visited
-        visited = {beginWord: 0}
+        # flag for when endWord is found
+        stop = False
         
-        q = [beginWord]
+        # all letters in the alphabet
+        alpha = 'qwertyuiopasdfghjklzxcvbnm'
+        
+        # first layer
+        layer = set([beginWord])
+        
+        # remove beginWord from wordList
+        wordList.discard(beginWord)
         
         # breadth first search to populate adjacency list with parent:child
-        while len(q) > 0:
-            word = q.pop(0)         
-            for i in range(len(word)):
-                tmp = word
-                for letter in range('a', 'z'):
-                    tmp[i] = letter
-
-                    if tmp in wordList:
-
-                        # only add if not same level or above
-                        if tmp not in visited or visited[tmp] > visited[word]:
-
-                            if word not in adjacency_map:
-                                adjacency_map[word] = [tmp]
-
-                            else:
-                                adjacency_map[word].append(tmp)
-                            visited[tmp] = visited[word] + 1
-                            q.append(tmp)
-                        
-        # depth first search for all paths to the final word
-        if endWord not in visited:
-            return []
-        
-        output = []
-        
-        stack = []
-        
-        def dfs(w, stack):
+        while layer and not stop:
+            next_layer = set()
             
+            for word in layer:
+                for i in range(len(word)):
+                    for letter in alpha:
+                        nw = word[:i] + letter + word[i + 1:]
+                        if nw in wordList:
+                            next_layer.add(nw)
+                            adjacency_map[word].add(nw)
+                        if nw == endWord:
+                            stop = True
+            wordList -= next_layer
+            layer = next_layer
+        
+        ans = []
+        path = [beginWord]
+        
+		# find the paths with dfs
+        def dfs(w):
             if w == endWord:
-                output.append(stack[:])
-                return
-            
-            stack.append(w)
-            
-            if w in adjacency_map:
-                for neighbor in adjacency_map[w]:
-                    dfs(neighbor, stack)
-
-            stack.pop()
-            
-        dfs(beginWord, stack)
+                ans.append(path[:])
+            else:
+                for nw in adjacency_map[w]:
+                    path.append(nw)
+                    dfs(nw)
+                    path.pop()
+                
+        dfs(beginWord)
         
-        return [list(t) for t in set(tuple(element) for element in output)]
+        return ans
